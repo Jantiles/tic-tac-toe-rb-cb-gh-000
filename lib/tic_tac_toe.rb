39f2d096334d41fb3bb59ad5753ece6fee3be1e0
Javier Antiles <javier_susy_nike@yahoo.com>
2018-01-19 23:27:09 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -49,11 +49,11 @@ end
 def turn_count(board)
   count = 0
   board.each do |str|
- +    if str.strip!=""
- +      count+=1
- +    end
- +  end
- +  return count
+     if str.strip!=""
+       count+=1
+     end
+   end
+   return count
 end
 
 def current_player(board)
